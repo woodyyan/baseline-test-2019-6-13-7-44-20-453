4c1c33fa48a89e0dba54a809c3a7e1aa8f3bb9d2
Songbai Yan <colorguitar@hotmail.com>
2019-07-02 14:43:38 +0800

    collection same elements from sub collections.

--- practice/section-1/practice-2.js
+++ practice/section-1/practice-2.js
@@ -1,5 +1,7 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  return '实现练习要求，并改写该行代码。';
+  return collectionA.filter(a => collectionB.reduce(function(a, b) {
+    return a.concat(b)
+  }, []).includes(a))
 }
