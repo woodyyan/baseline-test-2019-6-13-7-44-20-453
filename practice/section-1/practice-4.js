2f7d6d321aacd6cae849bf3eeb3d284ee0d9d823
Songbai Yan <colorguitar@hotmail.com>
2019-07-02 14:50:12 +0800

    collect same elemet from object property and collection key.

--- practice/section-1/practice-4.js
+++ practice/section-1/practice-4.js
@@ -1,5 +1,5 @@
 'use strict';
 
 function collectSameElements(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  return collectionA.filter(a => objectB.value.includes(a.key)).map(a => a.key)
 }
